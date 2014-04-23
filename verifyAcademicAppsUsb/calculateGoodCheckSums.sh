58f1a6f89552cb1f721aeb1d353690959fa4f73a
Greg Hilston <Grehgh@gmail.com>
2014-04-23 12:17:53 -0400

    Delete calculateGoodCheckSums.sh

--- verifyAcademicAppsUsb/calculateGoodCheckSums.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Academic Apps USB - Integrity Checker
-# Usage: This is used to generate a "goodCheckSums.md5"
-
-
-find . -type f -not -name "goodCheckSums.md5" -not -name "calculateVerifyCheckSums.sh" -not -name "calculateGoodCheckSums.sh" -not -name "verifyCheckSums.md5" -not -name "\.*" -exec md5sum '{}' \; > goodCheckSums.md5 # Mac's "md5sum" program is called md5, so change that to execute on a Mac
-
-echo Successfully generated goodCheckSums.md5
