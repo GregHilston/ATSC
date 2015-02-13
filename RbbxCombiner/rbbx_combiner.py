f6a22582ec15f5be359d23f6cece94e6ce60c4af
Greg Hilston <Gregory.Hilston@gmail.com>
2015-02-13 12:58:36 -0500

    Supporting different file names
    Script now supports file names with (1-75) and what not at the end and
    also a random character. In reality, supports anything with the correct
    date format as /d*_/d*_/d*

--- RbbxCombiner/rbbx_combiner.py
+++ RbbxCombiner/rbbx_combiner.py
@@ -7,11 +7,13 @@ Script used to combine multiple .rbbx files into one, allowing human interaction
 import sys
 import fileinput
 import time
+import re
 from xml.etree import ElementTree
 
 __author__ = 'Tyler_And_Greg' # Best ever
-combined_files_limit = 3  # Number of students should be this number * 75
-DEBUG_MODE = 1
+combined_files_limit = 3  # Max input files per one combined file (Note: Number of students should be this number * 75)
+DEBUG_MODE = 0 # Displays more information
+combined_counter = 1 # Number of combined files being created or about to be created
 
 '''
 As it stands, blackboard has a limitation of a student size xml file (Which is referred to as .rbbx files) of 400.
@@ -20,17 +22,30 @@ The program accepts any number of .rbbx files and combines them into files of 37
 5 files into one.
 '''
 
-
 # Remove all "nso:" namespace from the file
 def remove_ns0(file):
     for line in fileinput.FileInput(file, inplace=1):
         line = line.replace("ns0:", "")
         print line,  # Printed on same line
 
+# Based on the input file name and counters, generate an appropriate output file name
+def generate_output_filename(filename):
+    p = re.compile("\d*_\d*_\d*") # Construct a regex for the date
+    match = p.search(filename) # Search the filename for the string that is the date
+    
+    modified_filename = filename.rsplit('\\', 1)[1] # Remove the file path
+    modified_filename = re.compile("\d*_\d*_\d*").split(modified_filename)[0] # Use regex to remove the date and everything after (Ideally would just remove what's after it...)
+    modified_filename = modified_filename + match.group(0) # Reattach the date, as the split removed it as the delimiter
+    modified_filename = str(combined_counter) + "_" + modified_filename + ".rbbx" # To differentiate multiple files
+    
+    if DEBUG_MODE:
+            print "modified_filename: " + modified_filename
+            
+    return modified_filename
 
 # Given any number of .rbbx files, combines up to five .rbbx files into one, resulting in files of 375 students or less
 def combine(files):
-    combined_counter = 1 # Number of combined files being created or about to be created
+    global combined_counter # Needed to modify global variable
     file_counter = 0 # What .rbbx file we are currently on
     first = None # Our ElementTree
     combined_filenames = set() # To keep only unique filenames
@@ -39,45 +54,46 @@ def combine(files):
         data = ElementTree.parse(filename).getroot()
 
         if first is None:
-            first = data
+            first = data # Writing to a new file
         else:
-            first.extend(data)
-
-        #  Writing to file
-        modified_filename = filename.rsplit('\\', 1)[1] # Remove the file path
-        modified_filename = modified_filename.split(" (")[0] + ".rbbx" # Remove the student numbering
-        modified_filename = str(combined_counter) + "_" + modified_filename # To differentiate multiple files 
+            first.extend(data) # Extending the current file
+       
+        modified_filename = generate_output_filename(filename) # Get the output file name
         
-        if DEBUG_MODE:
-            print "modified_filename: " + modified_filename
-        
+        # Actually write to the file
         file = open((modified_filename), 'w+')
-        
         file.write(ElementTree.tostring(first))
         file.close()
         file_counter += 1
 
+        # Handle having to write to a new file, if we push the student limit for the current file
         if file_counter % combined_files_limit == 0:
             combined_filenames.add(modified_filename) # Done with this file, so add it to our list
             combined_counter += 1
             first = None # Empties the element tree, as we would push over 400 students
             
     combined_filenames.add(modified_filename) # Adds the last file, set ensures uniqueness
-
     
-    print "combined_counter: " + str(combined_counter)
+    print "Output:"
     for file in combined_filenames:
         if DEBUG_MODE:
             print "\tPassing to remove_ns0: " + file
-            
+        
         remove_ns0(file)
-
+        print("\t" + file)
+        
+    print "***********************************************"
 
 if  __name__ =='__main__':
     if DEBUG_MODE:
-        # Printing all arguements passed in
+        # Printing all arguments passed in
         print "len(sys.argv): " + str(len(sys.argv))
         for arg in sys.argv:
             print "\t" + arg
+            
+    # Printing some other useful information regarding student limits per each file
+    print "***********************************************"
+    print "Each combined file contains <= " + str(combined_files_limit * 75) + " students"
+    print "\tMax " + str(combined_files_limit) + " input files per combined file\n"
         
-    combine(sys.argv[1:]) # -2: 1 for the filename of this script, 2 for the weird .rbbx0 passed by windows
+    combine(sys.argv[1:]) # Pass all input except the name of this script
